7544e2e718806e86217832ef8da85ecd4b8a18f1
GabrielGustavoMS <55142297+GabrielGustavoMS@users.noreply.github.com>
2021-12-19 18:08:07 -0300

    Footer, Btn Top

--- web/NLW/beauty-salon/_js/main.js
+++ web/NLW/beauty-salon/_js/main.js
@@ -21,21 +21,6 @@ for (const link of links) {
   })
 }
 
-//Mudar o header da página quando der o scroll
-
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
-
-window.addEventListener('scroll', function () {
-  if (window.scrollY >= navHeight) {
-    // scroll é maior que a altura do header
-    header.classList.add('scroll')
-  } else {
-    // menor que a altura do header
-    header.classList.remove('scroll')
-  }
-})
-
 /* Testimonials carousel slider swiper */
 const swiper = new Swiper('.swiper', {
   slidesPerView: 1,
@@ -64,9 +49,39 @@ scrollReveal.reveal(
    #about .image, #about .text, 
    #services header, #services .card,
    #testimonials header, #testimonials .testimonials,
-   #contact .text, #contact .links
+   #contact .text, #contact .links,
+   footer .brand, footer .social
 `,
   {
     interval: 100
   }
 )
+const navHeight = header.offsetHeight
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    // menor que a altura do header
+    header.classList.remove('scroll')
+  }
+}
+
+function backToTop() {
+  // BTN Voltar para o topo
+
+  const backToTopButton = document.querySelector('#back-to-top')
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+//When Scroll
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+})
