65b99e17e7e91d0f480bd59c8739d10c04d67662
GabrielGustavoMS <55142297+GabrielGustavoMS@users.noreply.github.com>
2021-12-19 12:46:16 -0300

    Aula 02

---
+++ web/NLW/beauty-salon/main.js
@@ -0,0 +1,37 @@
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    // A função toggle - Se tiver a classe show tira
+    // Se não adiciona
+    nav.classList.toggle('show')
+  })
+}
+
+// Quando clicar em um item do menu, fechar o menu
+
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    //Utiliza-se remove no lugar de toggle, pois
+    //o menu já está aberto
+    nav.classList.remove('show')
+  })
+}
+
+//Mudar o header da página quando der o scroll
+
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    // menor que a altura do header
+    header.classList.remove('scroll')
+  }
+})
